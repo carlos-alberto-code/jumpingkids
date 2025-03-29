c333eebc98a4ef4962c62b513e0f326ed69c602e
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-03-29 23:30:56 +0000

    Modificación del side bar

--- ui/view/components/sidebar/sidebar.py
+++ ui/view/components/sidebar/sidebar.py
@@ -6,9 +6,10 @@ from ui.view.components.sidebar.sidebar_item import SidebarItem
 
 class Sidebar(ft.Container):
     
-    def __init__(self, modules: list[SidebarItem], on_select=None) -> None:
+    def __init__(self, user_name: str, sidebar_items: list[SidebarItem], on_select=None) -> None:
+        self._user_name = user_name
         self._on_select_event = on_select
-        self._modules = modules
+        self._modules = sidebar_items
         self._module_active = self._modules[0]
         super().__init__(
             content=ft.Column(
@@ -17,13 +18,13 @@ class Sidebar(ft.Container):
                         content=ft.Row(
                             controls=[
                                 ft.Icon(ft.Icons.ACCOUNT_CIRCLE, color=ft.Colors.WHITE),
-                                ft.Text("Isabel Serrano", color=ft.Colors.WHITE, size=18, weight=ft.FontWeight.BOLD),
+                                ft.Text(user_name, color=ft.Colors.WHITE, size=18, weight=ft.FontWeight.BOLD),
                             ]
                         ),
                         padding=10,
                     ),
                     ft.Divider(color=ft.Colors.WHITE24),
-                    *modules,
+                    *sidebar_items,
                     ft.Divider(color=ft.Colors.WHITE24),
                     SidebarItem("Settings", ft.Icons.SETTINGS, on_click=lambda _: print("Settings clicked")),
                     SidebarItem("Help", ft.Icons.HELP, on_click=lambda _: print("Help clicked")),
@@ -38,3 +39,7 @@ class Sidebar(ft.Container):
             expand=True,
             
         )
+    
+    @property
+    def user_name(self) -> str:
+        return self._user_name
