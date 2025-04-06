1d3695ab4c954401acb6be0fd6e87a517bbb0b83
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-04-06 20:36:47 +0000

    Se agregó la dependencia para manejar el sidebar.

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import flet as ft
 
-from ui.app_builder import AppViewBuilder
-from ui.view.theme import JumpingKidsTheme
+from ui.kids.app_builder import AppViewBuilder
+from ui.kids.view.theme import JumpingKidsTheme
+
+from navigation_system.widget.sidebar import Sidebar, SidebarContent, SidebarGroup, SidebarItem
 
 
 def main(page: ft.Page):
 
+    page.padding = 0
     page.theme = JumpingKidsTheme()
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    app_builder = AppViewBuilder()
-    page.views.extend(view for _, view in app_builder)
-    page.update()
+    sidebar_content = SidebarContent(
+        groups=[
+            SidebarGroup(
+                title="Kids",
+                items=[
+                    SidebarItem(
+                        icon=ft.Icons.HOME,
+                        name="Home",
+                    ),
+                    SidebarItem(
+                        icon=ft.Icons.PERSON,
+                        name="Profile",
+                    ),
+                    SidebarItem(
+                        icon=ft.Icons.SETTINGS,
+                        name="Settings",
+                    ),
+                ],
+            )
+        ]
+    )
+    page.add(
+        Sidebar(
+            sidebar_content=sidebar_content,
+            company_name="Jumpingkids",
+        )
+    )
 
 
-ft.app(target=main)
+ft.app(target=main, port=9000)
