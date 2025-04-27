f694ac7d768a874969b744c06d5ccbf209a9207c
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-04-27 14:17:23 +0000

    Mejora de la inicialización de los modelos de base de datos.

--- infrastructure/database/model.py
+++ infrastructure/database/model.py
@@ -6,20 +6,20 @@ class Base(DeclarativeBase):
     pass
 
 
-class SubscriptionEntity(Base):
-    """
-    Representa una suscripción a un servicio. Cada suscripción tiene un nombre, una fecha de inicio y una fecha de finalización. La fecha de finalización se calcula automáticamente al agregar 30 días a la fecha de inicio.
-    """
-    __tablename__ = "subscriptions"
+class SubscriptionTypeEntity(Base):
+    __tablename__ = "subscription_types"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
 
+    def __init__(self, name: str):
+        self.name = name
+    
+    def __repr__(self):
+        return f"SubscriptionTypeEntity(name={self.name})"
+
 
 class CategoryEntity(Base):
-    """
-    Representa una categoría de rutina. Ejemplos de categorías de rutina incluyen Yoga, Spinning, Crossfit, Fuerza, Resistencia, etc. Cada categoría puede tener múltiples rutinas asociadas.
-    """
     __tablename__ = "categories"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -27,14 +27,14 @@ class CategoryEntity(Base):
 
     routines: Mapped[list["RoutineEntity"]] = relationship("RoutineEntity", secondary="categories_routines", back_populates="categories", lazy="selectin")
 
+    def __init__(self, name: str):
+        self.name = name
+
     def __repr__(self):
         return f"CategoryEntity(id={self.id}, name={self.name})"
 
 
 class RoutineEntity(Base):
-    """
-    Representa una rutina de ejercicio. Cada rutina puede tener un nombre, una descripción y una categoría asociada. Las rutinas pueden estar asociadas con múltiples ejercicios.
-    """
     __tablename__ = "routines"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -43,6 +43,10 @@ class RoutineEntity(Base):
 
     categories: Mapped[list["CategoryEntity"]] = relationship("CategoryEntity", secondary="categories_routines", back_populates="routines", lazy="selectin")
     exercises: Mapped[list["ExerciseEntity"]] = relationship("ExerciseEntity", secondary="routines_exercises", lazy="selectin")
+
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
 
     def __repr__(self):
         return f"RoutineEntity(id={self.id}, name={self.name})"
@@ -57,14 +61,15 @@ CategoryRoutineEntity = Table(
 
 
 class ExerciseEntity(Base):
-    """
-    Representa un ejercicio individual. Cada ejercicio puede tener un nombre y una descripción. Los ejercicios pueden estar asociados con múltiples rutinas.
-    """
     __tablename__ = "exercises"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
 
     def __repr__(self):
         return f"ExerciseEntity(id={self.id}, name={self.name})"
@@ -78,28 +83,28 @@ RoutineExerciseEntity = Table(
 )
 
 class TutorEntity(Base):
-    """
-    Representa un tutor o cuidador de un niño. Cada tutor puede tener un nombre completo y puede estar asociado con múltiples niños.
-    """
     __tablename__ = "tutors"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     full_name: Mapped[str] = mapped_column(String(100), nullable=False)
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    subscription_type_id: Mapped[int] = mapped_column(Integer, ForeignKey("subscriptions.id"), nullable=False)
-    subscription: Mapped["SubscriptionEntity"] = relationship("SubscriptionEntity", lazy="selectin")
+    subscription_type_id: Mapped[int] = mapped_column(Integer, ForeignKey("subscription_types.id"), nullable=False)
 
     children: Mapped[list["ChildEntity"]] = relationship("ChildEntity", back_populates="tutor", lazy="selectin")
+    subscription_type: Mapped[SubscriptionTypeEntity] = relationship("SubscriptionTypeEntity", lazy="selectin")
+
+    def __init__(self, full_name: str, username: str, password: str, subscription_type_id: int):
+        self.full_name = full_name
+        self.username = username
+        self.password = password
+        self.subscription_type_id = subscription_type_id
 
     def __repr__(self):
         return f"TutorEntity(id={self.id}, full_name={self.full_name})"
 
 
 class ChildEntity(Base):
-    """
-    Representa a un niño que realiza las rutinas. Cada niño puede tener un nombre completo, una edad y un tutor asociado. Los niños pueden estar asociados con múltiples rutinas a través de la tabla de asignación.
-    """
     __tablename__ = "children"
     
 
@@ -108,10 +113,15 @@ class ChildEntity(Base):
     tutor_id: Mapped[int] = mapped_column(Integer, ForeignKey("tutors.id"), nullable=False)
     username: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    subscription_type: Mapped[SubscriptionEntity] = mapped_column(Integer, ForeignKey("subscriptions.id"), nullable=False)
 
     tutor: Mapped["TutorEntity"] = relationship("TutorEntity", back_populates="children", lazy="selectin")
     favorite_routines: Mapped[list["RoutineEntity"]] = relationship("RoutineEntity", secondary="favorite_routines",lazy="selectin")
+
+    def __init__(self, full_name: str, tutor_id: int, username: str, password: str):
+        self.full_name = full_name
+        self.tutor_id = tutor_id
+        self.username = username
+        self.password = password
 
     def __repr__(self):
         return f"ChildEntity(id={self.id}, full_name={self.full_name})"
