faaed23aa2f40a697f52f1e70c50aab99e0b4247
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-04-28 23:01:12 +0000

    Eliminación de archivos ya no necesarios por refactorización

--- domain/login/application/__init__.py
+++
@@ -1,9 +0,0 @@
-from domain.login.application.login_service_port import LoginServicePort
-from domain.login.application.login_service_core import LoginServiceCore
-from domain.login.application.login_repository_port import LoginRepositoryPort
-
-___all__ = [
-    "LoginServicePort",
-    "LoginServiceCore",
-    "LoginRepositoryPort",
-]
