7033dc9122606252332c774bbf2a0e75b18c6d79
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-04-30 17:19:50 +0000

    Eliminación de archivos por refactorización

--- navigation_system/interface/repository.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Repository(ABC):
-    """
-    Base para todos los repositorios que manejan la persistencia de datos. Todas las clases que representarán un repositorio deben heredar de esta clase.
-    """
-    pass
