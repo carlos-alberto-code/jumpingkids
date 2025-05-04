a6268d6ff764be4d4cc9320ea2abf2df3fb500c5
Carlos Alberto Baltazar Hinojosa <97766052+carlos-alberto-code@users.noreply.github.com>
2025-05-04 05:49:48 +0000

    Add AppState class for managing application state; integrate user and app properties in main function for improved state handling

---
+++ app_state.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+from domain.app import App
+from domain.model import User
+
+
+class AppState:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, user: User, app: App):
+        if not hasattr(self, '_initialized'):
+            self._user = user
+            self._app = app
+        
+    @property
+    def user(self) -> User:
+        return self._user
+    
+    @user.setter
+    def user(self, value: User) -> None:
+        self._user = value
+
+    @property
+    def app(self) -> App:
+        return self._app
+
+    @app.setter
+    def app(self, value: App) -> None:
+        self._app = value
--- main.py
+++ main.py
@@ -1,28 +1,66 @@
 import flet as ft
 
 from domain.login import LoginServiceCore
-from infrastructure.login import LoginRepositoryAdapter
 from domain.subscription import SubscriptionServiceCore
+
+from infrastructure.login import LoginRepositoryAdapter
+
+from ui.app.login.login_view import LoginView
 from ui.adapter.subscription_gui_adapter import SubscriptionGuiAdapter
+
+from app_state import AppState
+
+
+def show_snackbar_message(message: str, color: ft.Colors):
+    ...
 
 
 def main(page: ft.Page):
 
     page.padding = 0
     page.theme_mode = ft.ThemeMode.LIGHT
-
-    user = LoginServiceCore(LoginRepositoryAdapter()).login("cabh", "cabh")
-    print(user)
-
-    if not user:
-        raise ValueError("No se pudo iniciar sesión con las credenciales proporcionadas.")
     
-    app = SubscriptionServiceCore(SubscriptionGuiAdapter()).get_user_app(user)
-    
-    view_manager = app.view_manager
-    page.theme = app.theme
-    content = view_manager["home"]
-    page.add(content)
+    sidebar = Sidebar()
 
+    def on_login(event: ft.ControlEvent):
+        if login_view.not_complet_data:
+            show_snackbar_message(
+                message="Por favor, completa todos los campos.",
+                color=ft.Colors.RED
+            )
+
+        login_service = LoginServiceCore(LoginRepositoryAdapter())
+        user = login_service.login(login_view.username_field, login_view.password_field)
+
+        if not user:
+            show_snackbar_message(
+                message="Usuario no encontrado!",
+                color=ft.Colors.RED
+            )
+
+        subscription_service = SubscriptionServiceCore(SubscriptionGuiAdapter())
+
+        state = AppState(user=user, app=subscription_service.get_user_app(user))
+    
+        page.theme = state.app.theme
+        sidebar.keys = state.app.views.keys()
+
+        page.views.clear()
+        default_view = state.app.views[0]
+        page.views.append(default_view)
+        page.go(default_view.route)
+        page.update()
+    
+    def on_signup(event: ft.ControlEvent):
+        print("Signup button clicked")
+
+    login_view = LoginView(
+        on_login=on_login,
+        on_signup=on_signup
+    )
+
+    page.views.append(login_view)
+    page.go("/login")
+    page.update()
 
 ft.app(target=main, port=9000)
